e7def1e2a08751c0eac6fdaf51a7966aa178d880
orbitingflea <orbitingflea@qq.com>
2021-10-17 22:44:44 +0800

    upload

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vlf selectrum with-editor websocket request polymode deferred dash anaphora)))
+   '(markdown-mode vue-mode vlf selectrum with-editor websocket request polymode deferred dash anaphora)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
